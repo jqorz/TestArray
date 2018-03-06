8e9edc343b3a5527db319f8bf5188fe104cb9dcf
jqorz <jia13084056886>
2018-03-06 10:20:20 +0800

    1.增加新的手写算法（不可用）

--- app/src/main/java/com/jqorz/test/write/WriteActivity.java
+++ app/src/main/java/com/jqorz/test/write/WriteActivity.java
@@ -2,17 +2,20 @@ package com.jqorz.test.write;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.jqorz.test.R;
 import com.jqorz.test.write.write.WriteView;
 import com.jqorz.test.write.write.WriteView1;
 import com.jqorz.test.write.write.WriteView2;
+import com.jqorz.test.write.write.WriteView3;
 
 public class WriteActivity extends AppCompatActivity {
     WriteView mWriteView;
     WriteView1 mWriteView1;
     WriteView2 mWriteView2;
+    WriteView3 mWriteView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +24,30 @@ public class WriteActivity extends AppCompatActivity {
         mWriteView = findViewById(R.id.mWriteView);
         mWriteView1 = findViewById(R.id.mWriteView1);
         mWriteView2 = findViewById(R.id.mWriteView2);
+        mWriteView3 = findViewById(R.id.mWriteView3);
 
     }
 
-    public void onClickClearBtn(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_clear:
+                onClickClearBtn();
+                break;
+        }
+        return false;
+    }
+
+    public void onClickClearBtn() {
         mWriteView.clear();
         mWriteView1.clear();
         mWriteView2.clear();
+        mWriteView3.clear();
     }
 }
--- app/src/main/java/com/jqorz/test/write/write/WriteView3.java
+++ app/src/main/java/com/jqorz/test/write/write/WriteView3.java
@@ -18,9 +18,10 @@ import android.view.VelocityTracker;
 
 /**
  * 用于测试可变粗细的圆滑画笔
+ * 使用draw
  * 使用单个缓存点，粗细可变
  */
-public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
+public class WriteView3 extends SurfaceView implements SurfaceHolder.Callback {
     private static final float TOUCH_TOLERANCE = 5.5f;
     // 画笔，定义绘制属性
     private Paint mPaint;
@@ -29,16 +30,16 @@ public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
     // 画布及其底层位图
     private Bitmap mBitmap;
     private Canvas mCanvas;
-    private float paintWidthPrevious;//缓存上个path的画笔宽度
+    private float paintWidthPrevious = 12f;//缓存上个path的画笔宽度
     private boolean isNew = true;//标记，如果是新的一笔，则不需要画笔渐变
 
     private SurfaceHolder surfaceHolder;
 
-    private PointF point_last, point_middle;
+    private PointF point_temp, point_last;
 
     private VelocityTracker mVelocityTracker;
 
-    public WriteView(Context context, AttributeSet attrs) {
+    public WriteView3(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.setZOrderOnTop(true);
         this.getHolder().setFormat(PixelFormat.TRANSLUCENT);//去掉SurfaceView默认黑色背景
@@ -56,7 +57,8 @@ public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
         mPaint.setColor(Color.BLACK);
-        mPaint.setStyle(Paint.Style.STROKE);
+//        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);//设置画笔填充
+
 //        mPaint.setStrokeJoin(Paint.Join.ROUND);//设置圆弧连接
         mPaint.setStrokeCap(Paint.Cap.ROUND);//设置圆线帽
         mPaint.setStrokeWidth(12);
@@ -64,13 +66,22 @@ public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
 
         mPath = new Path();
 
+        point_temp = new PointF(0, 0);
         point_last = new PointF(0, 0);
-        point_middle = new PointF(0, 0);
 
         surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);//绘制Bitamp的画布对象
+
+        mBitmap.eraseColor(Color.WHITE);
+
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -87,11 +98,14 @@ public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
                 break;
             case MotionEvent.ACTION_MOVE:
                 mVelocityTracker.computeCurrentVelocity(10);
-                float speed = (getSpeed(mVelocityTracker.getXVelocity(),
-                        mVelocityTracker.getYVelocity()) / 2f);
-                paintWidthPrevious = getPaintWidth(speed);
-                mPaint.setStrokeWidth(paintWidthPrevious);
-                touch_move(x, y);
+                float velocityX = mVelocityTracker.getXVelocity();
+                float velocityY = mVelocityTracker.getYVelocity();
+
+//                float speed = (getSpeed(mVelocityTracker.getXVelocity(),
+//                        mVelocityTracker.getYVelocity()) / 2f);
+//                paintWidthPrevious = getPaintWidth(speed);
+//                mPaint.setStrokeWidth(paintWidthPrevious);
+                touch_move(x, y, velocityX, velocityY);
                 break;
             case MotionEvent.ACTION_UP:
                 touch_up(x, y);
@@ -151,13 +165,14 @@ public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
         Canvas canvas = surfaceHolder.lockCanvas();
         //将路径绘制在自己的Bitmap上
 
+
         mCanvas.drawPath(mPath, mPaint);
         //将Bitmap绘制到界面上
         canvas.drawBitmap(mBitmap, 0, 0, mPaint);
 
         mPath.reset();
 
-        mPath.moveTo(point_last.x, point_last.y);
+        mPath.moveTo(point_temp.x, point_temp.y);
 
         // 直接绘制路径
 //        canvas.drawPath(mPath, mPaint);
@@ -169,33 +184,70 @@ public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
 
         mPath.moveTo(x, y);
 
+        point_temp.x = (int) x;
+        point_temp.y = (int) y;
+
         point_last.x = (int) x;
         point_last.y = (int) y;
-
-        point_middle.x = (int) x;
-        point_middle.y = (int) y;
 
 
     }
 
-    private void touch_move(float x, float y) {
-        if (getUsefulPoint(x, y, point_middle.x, point_middle.y)) {
+    private void touch_move(float x, float y, float velocityX, float velocityY) {
+        if (getUsefulPoint(x, y, point_last.x, point_last.y)) {
+
+            double st = Math.atan(velocityY / velocityX);//新点的瞬时速度的角度
+
+            DoublePointF lastDoublePointF = convert(point_last.x, point_last.y, paintWidthPrevious, st);//前一个点的宽线坐标
+            DoublePointF currentDoublePointF = convert((x + point_last.x)/2, (y + point_last.y)/2, paintWidthPrevious, st);//当前点的宽线坐标
+
+            //Path目前停留在point_temp.x, point_temp.y
+            //从缓存点连接到该点的宽线坐标的一端
+            mPath.lineTo(lastDoublePointF.getPointF1().x, lastDoublePointF.getPointF1().y);
+            //绘制一侧的贝塞尔曲线
+//            mPath.lineTo(currentDoublePointF.getPointF1().x, currentDoublePointF.getPointF1().y);
+            mPath.quadTo(point_temp.x, point_temp.y, currentDoublePointF.getPointF1().x, currentDoublePointF.getPointF1().y);
+            //封闭当前点的一端
+            mPath.lineTo(currentDoublePointF.getPointF2().x, currentDoublePointF.getPointF2().y);
+            //绘制另一侧的贝塞尔曲线
+//            mPath.lineTo(lastDoublePointF.getPointF2().x, lastDoublePointF.getPointF2().y);
+            mPath.quadTo(point_temp.x, point_temp.y, lastDoublePointF.getPointF2().x, lastDoublePointF.getPointF2().y);
+            //封闭缓存点的一端，结束
+            mPath.lineTo(point_last.x, point_last.y);
+
+//  mPath.quadTo(point_last.x, point_last.y, (x + point_last.x) / 2, (y + point_last.y) / 2);
 
 
-            mPath.quadTo(point_middle.x, point_middle.y, (x + point_middle.x) / 2, (y + point_middle.y) / 2);
-            point_last.x = (x + point_middle.x) / 2;
-            point_last.y = (y + point_middle.y) / 2;
+            point_temp.x = (x + point_last.x) / 2;
+            point_temp.y = (y + point_last.y) / 2;
 
-            point_middle.x = x;
-            point_middle.y = y;
+            point_last.x = x;
+            point_last.y = y;
             draw();
 
         }
 
     }
 
+    /**
+     * 将原始坐标转换为宽线坐标
+     *
+     * @param x     原始点坐标x
+     * @param y     原始点坐标y
+     * @param width 扩宽的线的宽度
+     * @param st    位移角度
+     */
+    private DoublePointF convert(float x, float y, float width, double st) {
+        double last_x1 = x - width * Math.cos(st);
+        double last_x2 = x + width * Math.cos(st);
+
+        double last_y1 = y - width * Math.cos(st);
+        double last_y2 = y + width * Math.cos(st);
+        return new DoublePointF(new PointF((float) last_x1, (float) last_y1), new PointF((float) last_x2, (float) last_y2));
+    }
+
     private void touch_up(float x, float y) {
-        mPath.lineTo(point_middle.x, point_middle.y);
+        mPath.lineTo(point_last.x, point_last.y);
     }
 
     /**
@@ -214,14 +266,12 @@ public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
         // .createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
         // mCanvas = new Canvas(mBitmap);
         // 清除方法2：将位图清除为白色
-        if (mBitmap != null) {
-            mBitmap.eraseColor(Color.WHITE);
-            // 两种清除方法都必须加上后面这两步：
-            // 路径重置
-            mPath.reset();
-            // 刷新绘制
-            draw();
-        }
+        mBitmap.eraseColor(Color.WHITE);
+        // 两种清除方法都必须加上后面这两步：
+        // 路径重置
+        mPath.reset();
+        // 刷新绘制
+        draw();
     }
 
     @Override
@@ -231,14 +281,29 @@ public class WriteView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        mCanvas = new Canvas(mBitmap);//绘制Bitamp的画布对象
 
-        mBitmap.eraseColor(Color.WHITE);
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
+    }
+
+    class DoublePointF {
+        private PointF pointF1;
+        private PointF pointF2;
+
+        public DoublePointF(PointF pointF1, PointF pointF2) {
+            this.pointF1 = pointF1;
+            this.pointF2 = pointF2;
+        }
+
+        public PointF getPointF1() {
+            return pointF1;
+        }
+
+        public PointF getPointF2() {
+            return pointF2;
+        }
     }
 }
