c34c2c626c44626b6cd1cf8a280855b854fb652d
jqorz <jia13084056886>
2018-04-03 14:53:39 +0800

    增加nanohttpd库（视频服务）

---
+++ app/src/main/java/com/jqorz/test/videoServer/VideoServer.java
@@ -0,0 +1,89 @@
+package com.jqorz.test.videoServer;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import fi.iki.elonen.NanoHTTPD;
+
+/**
+ * copyright datedu
+ *
+ * @author jqorz
+ * @since 2018/4/3
+ * 用于开放一个http端口，给PC端访问
+ */
+public class VideoServer extends NanoHTTPD {
+
+    public static final int DEFAULT_SERVER_PORT = 8080;
+    public static final String TAG = VideoServer.class.getSimpleName();
+
+    private static final String REQUEST_ROOT = "/";
+
+    private String mVideoFilePath;
+    private int mVideoWidth = 0;
+    private int mVideoHeight = 0;
+
+    public VideoServer(String filepath, int width, int height, int port) {
+        super(DEFAULT_SERVER_PORT);
+        mVideoFilePath = filepath;
+        mVideoWidth = width;
+        mVideoHeight = height;
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        Log.d(TAG, "OnRequest: " + session.getUri());
+        if (REQUEST_ROOT.equals(session.getUri())) {
+            return responseRootPage(session);
+        } else if (mVideoFilePath.equals(session.getUri())) {
+            return responseVideoStream(session);
+        }
+        return response404(session, session.getUri());
+    }
+
+    public Response responseRootPage(IHTTPSession session) {
+        File file = new File(mVideoFilePath);
+        if (!file.exists()) {
+            return response404(session, mVideoFilePath);
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append("<!DOCTYPE html><html><body>");
+        builder.append("<video ");
+        builder.append("width=" + getQuotaStr(String.valueOf(mVideoWidth)) + " ");
+        builder.append("height=" + getQuotaStr(String.valueOf(mVideoHeight)) + " ");
+        builder.append("controls>");
+        builder.append("<source src=" + getQuotaStr(mVideoFilePath) + " ");
+        builder.append("type=" + getQuotaStr("video/mp4") + ">");
+        builder.append("Your browser doestn't support HTML5");
+        builder.append("</video>");
+        builder.append("</body></html>\n");
+        return  newFixedLengthResponse(builder.toString());
+    }
+
+    public Response responseVideoStream(IHTTPSession session) {
+        try {
+            FileInputStream fis = new FileInputStream(mVideoFilePath);
+            return newChunkedResponse(Response.Status.OK, "video/mp4", fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return response404(session, mVideoFilePath);
+        }
+    }
+
+    public Response response404(IHTTPSession session, String url) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("<!DOCTYPE html><html><body>");
+        builder.append("Sorry, Can't Found " + url + " !");
+        builder.append("</body></html>\n");
+        return  newFixedLengthResponse(builder.toString());
+    }
+
+
+    protected String getQuotaStr(String text) {
+        return "\"" + text + "\"";
+    }
+
+}
