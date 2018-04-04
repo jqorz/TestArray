a2c6b754feaaa4f9020824faaba4ef72b86b2f2f
jqorz <jia13084056886>
2018-04-04 17:29:51 +0800

    增加点击测试

---
+++ app/src/main/java/com/jqorz/test/click/ClickActivity.java
@@ -0,0 +1,35 @@
+package com.jqorz.test.click;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.jqorz.test.R;
+import com.jqorz.test.ToastUtil;
+
+/**
+ * copyright datedu
+ *
+ * @author jqorz
+ * @since 2018/4/4
+ */
+public class ClickActivity extends AppCompatActivity {
+    private String TAG = "ClickActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click);
+    }
+
+    public void onClickParent(View v) {
+        Log.i(TAG, "click parent");
+        ToastUtil.showToast(this, "parent");
+    }
+
+    public void onClickChild(View v) {
+        Log.i(TAG, "click child");
+        ToastUtil.showToast(this, "child");
+    }
+}
