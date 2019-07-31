39e363c59c1dbce2f811724f655f9c83073f8693
jqorz <j1997321@qq.com>
2019-07-31 11:23:05 +0800

    增加webview拦截请求

--- app/src/main/java/com/jqorz/test/webview/WebViewActivity.java
+++ app/src/main/java/com/jqorz/test/webview/WebViewActivity.java
@@ -2,13 +2,27 @@ package com.jqorz.test.webview;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.jqorz.test.R;
 import com.jqorz.test.base.BaseActivity;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * copyright datedu
@@ -44,6 +58,44 @@ public class WebViewActivity extends BaseActivity {
         webSettings.setUseWideViewPort(true);
         webSettings.setLoadWithOverviewMode(true);
         mWebView.loadUrl(getIntent().getStringExtra(INTENT_URL));
+        mWebView.setWebViewClient(new WebViewClient() {
+            private String lastTime = "";
+
+            @Nullable
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                try {
+                    URL urlStr = new URL(request.getUrl().toString());
+                    URLConnection rulConnection = urlStr.openConnection();
+                    rulConnection.addRequestProperty("If-Modified-Since", lastTime);
+                    HttpURLConnection conn = (HttpURLConnection) rulConnection;
+                    // Read input
+                    String charset = conn.getContentEncoding() != null ? conn.getContentEncoding() : Charset.defaultCharset().displayName();
+                    String mime = conn.getContentType();
+                    InputStream isContents = conn.getInputStream();
+                    WebResourceResponse response = new WebResourceResponse(mime, charset, isContents);
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        //设置状态码
+                        response.setStatusCodeAndReasonPhrase(conn.getResponseCode(), conn.getResponseMessage());
+                        Map<String, List<String>> conResponseHeader = conn.getHeaderFields();
+                        Map<String, String> responseHeader = new HashMap<>();
+                        for (Map.Entry<String, List<String>> entry : conResponseHeader.entrySet()) {
+                            if (!TextUtils.isEmpty(entry.getKey())) {
+                                responseHeader.put(entry.getKey(), entry.getValue().get(0));
+                            }
+                        }
+                        response.setResponseHeaders(responseHeader);
+                    }
+                    String resTime = conn.getHeaderField("Last-Modified");
+                    lastTime = resTime;
+                    return response;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        });
     }
 
     @Override
