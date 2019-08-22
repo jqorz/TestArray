82b75e7ee26dee8e86344f32bdad5f74904557ae
jqorz <j1997321@qq.com>
2019-08-22 10:46:57 +0800

    1.增加仪表盘自定义view

---
+++ app/src/main/java/com/jqorz/test/dashboard/DashActivity.java
@@ -0,0 +1,21 @@
+package com.jqorz.test.dashboard;
+
+import com.jqorz.test.base.BaseActivity;
+
+/**
+ * copyright datedu
+ *
+ * @author jqorz
+ * @since 2019/8/22
+ */
+public class DashActivity extends BaseActivity {
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}
