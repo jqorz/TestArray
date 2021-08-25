f66bd00a173de85a0b284e20a50da36939fe8097
jqorz <j1997321@qq.com>
2021-08-25 23:05:25 +0800

    协程测试

--- app/src/main/java/com/jqorz/test/cortinue/CoroutineTest.kt
+++ app/src/main/java/com/jqorz/test/cortinue/CoroutineTest.kt
@@ -1,6 +1,7 @@
 package com.jqorz.test.cortinue
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.math.pow
@@ -12,31 +13,15 @@ import kotlin.math.pow
 object CoroutineTest {
     @JvmStatic
     fun main(args: Array<String>) {
-//        GlobalScope.launch(Dispatchers.Unconfined) {
-//            Log.d("AA", "协程初始化完成，时间: " + System.currentTimeMillis())
-//            for (i in 1..3) {
-//                Log.d("AA", "协程任务1打印第$i 次，时间: " + System.currentTimeMillis())
-//            }
-//            delay(500)
-//            for (i in 1..3) {
-//                Log.d("AA", "协程任务2打印第$i 次，时间: " + System.currentTimeMillis())
-//            }
-//        }
-//
-//        Log.d("AA", "主线程 sleep ，时间: " + System.currentTimeMillis())
-//        Thread.sleep(1000)
-//        Log.d("AA", "主线程运行，时间: " + System.currentTimeMillis())
-//
-//        for (i in 1..3) {
-//            Log.d("AA", "主线程打印第$i 次，时间: " + System.currentTimeMillis())
-//        }
-
         GlobalScope.launch {
             var s = ""
             for (i in 0..1000) {
                 s += "${i.toDouble().pow(100)}"
             }
             println("1.launch....[当前线程为：${Thread.currentThread().name}] ")
+            async {
+
+            }.await()
         }
         println("2.launch.... ")
 
